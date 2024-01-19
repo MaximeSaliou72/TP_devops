5cdf276ddc7d4455d8f5c4962f935ce179b391f7
MaximeSaliou72 <maximesaliou72@gmail.com>
2024-01-19 16:15:00 +0100

    rajout commentaire

--- __tests__/cart_test.js
+++ __tests__/cart_test.js
@@ -2,24 +2,32 @@ let Cart = require("../src/Cart.js");
 let Product = require("../src/Product.js");
 const InMemoryStorage = require('../src/InMemoryStorage');
 
-
+// Test d'achat d'un article
 test("TestBuy, achat d'un article", () => {
+    // Création d'un stockage en mémoire
     const storage = new InMemoryStorage();
+    // Création d'un panier avec le stockage
     const cart = new Cart(storage);
+    // Création d'un produit Product1 au prix de 10.0
     const product = new Product('Product1', 10.0);
+    // Achat de 3 exemplaires du produit
     cart.buy(product, 3);
+    // Vérification que le total du panier est de 30.0
     expect(cart.total()).toBe(30.0);
 });
 
+// Test de réinitialisation du panier
 test('testReset, reset le panier ', () => {
     const storage = new InMemoryStorage();
     const cart = new Cart(storage);
     const product = new Product('Product1', 10.0);
     cart.buy(product, 3);
+    // Réinitialisation du panier
     cart.reset();
     expect(cart.total()).toBe(0.0);
 });
 
+// Suppression d'un produit du panier
 test('testRestore, supprime un produit', () => {
     const storage = new InMemoryStorage();
     const cart = new Cart(storage);
@@ -27,13 +35,16 @@ test('testRestore, supprime un produit', () => {
     const product2 = new Product('Product2', 15.0);
     cart.buy(product1, 3);
     cart.buy(product2, 2);
+    // Suppréssion du produit2
     cart.restore(product2);
     expect(cart.total()).toBe(30.0);
 });
 
+// Test du calcul du total avec plusieurs produits
 test('testTotal, calcul le total de plusieurs produit', () => {
     const storage = new InMemoryStorage();
     const cart = new Cart(storage);
+    // Achat de 2 produits différent
     const product1 = new Product('Product1', 10.0);
     const product2 = new Product('Product2', 15.0);
     cart.buy(product1, 2);
--- __tests__/product_test.js
+++ __tests__/product_test.js
@@ -1,23 +1,23 @@
 
 let Product = require("../src/Product.js");
 
-test('getName function returns the correct name', () => {
+test('getName, renvoie le nom du produit', () => {
   const product = new Product('Product1', 10.0);
   expect(product.getName()).toBe('Product1');
 });
 
-test('getPrice function returns the correct price', () => {
+test('getPrice, renvoie le prix du produit', () => {
   const product = new Product('Product1', 10.0);
   expect(product.getPrice()).toBe(10.0);
 });
 
-test('setName function sets the correct name', () => {
+test('setName, change le nom du produit', () => {
   const product = new Product('Product1', 10.0);
   product.setName('NewProduct');
   expect(product.getName()).toBe('NewProduct');
 });
 
-test('setPrice function sets the correct price', () => {
+test('setPrice, change le prix du produit', () => {
   const product = new Product('Product1', 10.0);
   product.setPrice(15.0);
   expect(product.getPrice()).toBe(15.0);
